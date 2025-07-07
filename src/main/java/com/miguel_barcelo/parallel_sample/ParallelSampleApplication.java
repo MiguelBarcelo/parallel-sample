42d39b5a2101a2d677cb458ab18ebfdc40c19914
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-07 16:45:22 -0700

    fix: init commit
    - add Application

---
+++ src/main/java/com/miguel_barcelo/parallel_sample/ParallelSampleApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.parallel_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelSampleApplication.class, args);
+	}
+
+}
